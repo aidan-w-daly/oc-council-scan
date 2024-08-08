74900d1f90ab251e9bf191a41747f16a14f015e4
Aidan Daly <aidanwdaly@gmail.com>
2024-08-08 12:09:43 -0700

    implemented get_last_agenda_an, finds pdf link to most recent city council agenda + TODO for get_last_agenda_sa

--- app.py
+++ app.py
@@ -11,18 +11,32 @@ url_garden_grove = "https://agendasuite.org/iip/gardengrove/agendaitem/list" #No
 url_city_of_orange = "https://www.cityoforange.org/our-city/local-government/city-council/city-council-meetings/-selcat-32#eventcats_46_0_36" #List of city council meetings, agenda in each one
 url_huntington_beach = "https://huntingtonbeach.legistar.com/Calendar.aspx" #List of all meetings with agenda links
 
-page_anaheim = requests.get(url_anaheim)
-page_santa_ana = requests.get(url_santa_ana)
-page_garden_grove = requests.get(url_garden_grove)
-page_city_of_orange = requests.get(url_city_of_orange)
-page_huntington_beach = requests.get(url_huntington_beach)
 
-soup_an = BeautifulSoup(page_anaheim.content, "lxml")
-soup_sa= BeautifulSoup(page_santa_ana.content, "lxml")
-soup_gg = BeautifulSoup(page_garden_grove.content, "lxml")
-soup_co = BeautifulSoup(page_city_of_orange.content, "lxml")
-soup_hb = BeautifulSoup(page_huntington_beach.content, "lxml")
+def get_last_agenda_an():
+    page_anaheim = requests.get(url_anaheim)
+    soup_an = BeautifulSoup(page_anaheim.content, "lxml")
+    agenda = soup_an.find('a', class_='Hyperlink', title='Print Current Agenda')
+    print(agenda['href'])
 
-an_results = soup_an.find(id="Meetings")
-print(an_results)
-print("end")
+#TODO website has two sections: "Current and Upcoming meetings" and "archived. I don't know how to navigate archive to most recent
+def get_last_agenda_sa():
+    page_santa_ana = requests.get(url_santa_ana)
+    soup_sa= BeautifulSoup(page_santa_ana.content, "lxml")
+
+#TODO
+def get_last_agenda_gg():
+    page_garden_grove = requests.get(url_garden_grove)
+    soup_gg = BeautifulSoup(page_garden_grove.content, "lxml")
+
+#TODO
+def get_last_agenda_co():
+    page_city_of_orange = requests.get(url_city_of_orange)
+    soup_co = BeautifulSoup(page_city_of_orange.content, "lxml")
+
+#TODO
+def get_last_agenda_hb():
+    page_huntington_beach = requests.get(url_huntington_beach)
+    soup_hb = BeautifulSoup(page_huntington_beach.content, "lxml")
+
+get_last_agenda_an()
+
