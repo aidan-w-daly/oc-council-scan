17b35f540e0a12cf2921d97e1021a0a2202cb83d
Aidan Daly <aidanwdaly@gmail.com>
2024-08-13 18:59:17 -0700

    Completed get_last_agenda_sa() method

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.support.ui import Select
 
 
-print("start")
+print("Running...")
 
 #Where do new city council agendas get uploaded? + In what format?
 url_anaheim = "https://www.anaheim.net/2527/Agendas" #list of city council agenda links, + viewer
@@ -23,43 +27,33 @@ def get_last_agenda_an():
 
     print(agenda['href'])
 
-#Website has possible locations for most recent agenda: tables "Current and Upcoming meetings" and "archived".
-# TODO the table bodies <tbody> are inaccessible with beautifulsoup alone, need selenium
-# TODO Locate table cell <td> which contains meeting title "Regular City Council."
-# TODO Get date from sibling cell <td> (common parent is row <tr>) 
-# TODO Get agenda link <a> from sibling cell <td> (from same parent row <tr>)
+#Selenium
 def get_last_agenda_sa():
-    # header_mask =   {
-    #                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0',
-    #                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
-    #                 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-    #                 'Accept-Encoding': 'gzip, deflate, br, zstd',
-    #                 'Accept-Language': 'en-US,en;q=0.5',
-    #                 'Connection': 'keep-alive'
-    #                 }
-    # page_santa_ana = requests.get(url_santa_ana)
-    # soup_sa = BeautifulSoup(page_santa_ana.content, "lxml")
-
-    # #search by title cell <td>, then get parent row <tr>, to get sibling agenda link <a>
-    # archived_table = soup_sa.find_all('td', string='Regular City Council and Housing Authority Meeting')
-    # print(archived_table)
-
-    #SELENIUM:
-    #bs4 thinks archived_table.tbody is childless, even with http header. need selenium to access this <tbody> and its data <td>
-    #It is loaded via JS, check network tab of FF devtools
-    driver = webdriver.Firefox()
+    options = FirefoxOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
     driver.get(url_santa_ana)
 
-    time.sleep(5) #TODO sleep doesn't work every time, replace with wait for element to load
-    #agenda_title_cell = driver.find_element(By.XPATH, '//table[@id="archivedMeetingsTable"]/tbody/tr/td[contains(text(),"Regular City Council")]')
-    agenda_title_cell = driver.find_element(By.XPATH, '//tr/td[contains(text(),"Regular City Council")]')
-    agenda_date_cell = agenda_title_cell.find_element(By.XPATH, './following-sibling::*[1]')
-    agenda_link = agenda_title_cell.find_element(By.XPATH, '..//a[text()="Agenda"]')
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//select[@name="archivedMeetingsTable_length"]'))
+        )
+        selection = Select(driver.find_element(By.XPATH, '//select[@name="archivedMeetingsTable_length"]'))
+        selection.select_by_value("15")
 
-    print(agenda_title_cell.text)
-    print(agenda_date_cell.text)
-    print(agenda_link.get_attribute('href'))
-    driver.close()
+        agenda_title_cell = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//tr/td[contains(text(),"Regular City Council")]'))
+        )
+        agenda_date_cell = agenda_title_cell.find_element(By.XPATH, './following-sibling::*[1]')
+        agenda_link = agenda_title_cell.find_element(By.XPATH, '..//a[text()="Agenda"]')
+
+        print(agenda_title_cell.text)
+        print(agenda_date_cell.text)
+        print(agenda_link.get_attribute('href'))
+    except:
+        print("An exception occurred.")
+    finally:
+        driver.quit()
 
 #TODO
 def get_last_agenda_gg():
@@ -76,5 +70,5 @@ def get_last_agenda_hb():
     page_huntington_beach = requests.get(url_huntington_beach)
     soup_hb = BeautifulSoup(page_huntington_beach.content, "lxml")
 
-get_last_agenda_sa()
+get_last_agenda_an()
 
